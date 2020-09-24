6fc4eb17ff159be6e42f3597f3d346d1f2ead303
Evan Haas <evan@lagerdata.com>
2020-09-24 11:34:47 -0700

    add assertion

--- python_tests/tester.py
+++ python_tests/tester.py
@@ -1,4 +1,15 @@
 import sys
+from lager import lager
+import serial
 
-print('Hello, world!')
-sys.exit(0)
+def main():
+    gateway = lager.Lager()
+    device = gateway.connect("nrf52", ignore_if_connected=True)
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=2) as ser:
+        device.reset(halt=False)
+        output = ser.read(512)
+
+    assert b'test_not_check_min_number_blinks' in output
+
+if __name__ == '__main__':
+    main()
