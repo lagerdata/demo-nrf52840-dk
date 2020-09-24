8888b98f3d4e9e0a5a70a02874d86bed431cebbe
Evan Haas <evan@lagerdata.com>
2020-09-24 11:37:23 -0700

    use pytest

--- python_tests/tester.py
+++ python_tests/tester.py
@@ -1,8 +1,18 @@
 import sys
+import pytest
 from lager import lager
 import serial
 
-def main():
+def test_good_serial_output():
+    gateway = lager.Lager()
+    device = gateway.connect("nrf52", ignore_if_connected=True)
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=2) as ser:
+        device.reset(halt=False)
+        output = ser.read(512)
+
+    assert b'test_check_min_number_blinks' in output
+
+def test_bad_serial_output():
     gateway = lager.Lager()
     device = gateway.connect("nrf52", ignore_if_connected=True)
     with serial.Serial('/dev/ttyACM0', 115200, timeout=2) as ser:
@@ -12,4 +22,4 @@ def main():
     assert b'test_not_check_min_number_blinks' in output
 
 if __name__ == '__main__':
-    main()
+   sys.exit(pytest.main([__file__]))
